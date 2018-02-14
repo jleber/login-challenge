7714613c4862234b7e8073fd61df33e1d097a31c
João Leber <joao.leber91@gmail.com>
2018-02-14 17:23:55 -0200

    Adding User Authentication endpoint

--- src/main/java/com/jleber/login/challenge/controller/UserController.java
+++ src/main/java/com/jleber/login/challenge/controller/UserController.java
@@ -1,7 +1,20 @@
 package com.jleber.login.challenge.controller;
 
-/**
- * Created by jleber on 14/02/18.
- */
+import com.jleber.login.challenge.request.UserRequest;
+import com.jleber.login.challenge.response.UserResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Controller
+@RequestMapping("/users")
 public class UserController {
+
+    @PostMapping(value = "/authentication", consumes = APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserResponse> registration(UserRequest userRequest) {
+        return null;
+    }
 }
