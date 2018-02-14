7b57f439b213444f42c34b3e031049666af5efd8
João Leber <joao.leber91@gmail.com>
2018-02-14 17:22:46 -0200

    Adding Initial Entity

--- src/main/java/com/jleber/login/challenge/model/Company.java
+++ src/main/java/com/jleber/login/challenge/model/Company.java
@@ -1,7 +1,29 @@
 package com.jleber.login.challenge.model;
 
-/**
- * Created by jleber on 14/02/18.
- */
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "company")
 public class Company {
+
+    @Column(name = "id_company")
+    private Long idCompany;
+
+    private String name;
+    private String phone;
+    private String address;
+
+    @Column(name = "address_number")
+    private String addressNumber;
+
+    @Column(name = "additional_info")
+    private String additionalInfo;
 }
--- src/main/java/com/jleber/login/challenge/model/User.java
+++ src/main/java/com/jleber/login/challenge/model/User.java
@@ -1,7 +1,24 @@
 package com.jleber.login.challenge.model;
 
-/**
- * Created by jleber on 14/02/18.
- */
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "user")
 public class User {
+
+    private String email;
+    private String name;
+    private String password;
+    private String token;
+
+    @ManyToOne(fetch= FetchType.LAZY)
+    @JoinColumn(name="id_company", nullable=false)
+    private Company company;
 }
