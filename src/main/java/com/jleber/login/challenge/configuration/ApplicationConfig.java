1e5686d65a09ad5f8d49f0425329ee66bcc54565
João Leber <joao.leber91@gmail.com>
2018-02-14 15:37:17 -0200

    Adding Spring boot config

---
+++ src/main/java/com/jleber/login/challenge/configuration/ApplicationConfig.java
@@ -0,0 +1,7 @@
+package com.jleber.login.challenge.configuration;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationConfig {
+}
