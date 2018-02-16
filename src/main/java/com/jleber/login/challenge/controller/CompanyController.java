399b1c85cd1fedee7dad4682dae63c4666e89607
João Leber <joao.leber91@gmail.com>
2018-02-16 13:54:55 -0200

    Adding refatoring and adding new Controller for company

--- src/main/java/com/jleber/login/challenge/controller/CompanyController.java
+++ src/main/java/com/jleber/login/challenge/controller/CompanyController.java
@@ -1,7 +1,26 @@
 package com.jleber.login.challenge.controller;
 
-/**
- * Created by jleber on 16/02/18.
- */
+import com.jleber.login.challenge.response.CompanyResponse;
+import com.jleber.login.challenge.service.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/companies")
 public class CompanyController {
+
+    @Autowired
+    private CompanyService companyService;
+
+    @GetMapping
+    public ResponseEntity<CompanyResponse> findCompany(HttpServletRequest request){
+        CompanyResponse response = companyService.findCompany(request.getHeader("api_token"));
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 }
--- src/main/java/com/jleber/login/challenge/controller/UserController.java
+++ src/main/java/com/jleber/login/challenge/controller/UserController.java
@@ -2,19 +2,27 @@ package com.jleber.login.challenge.controller;
 
 import com.jleber.login.challenge.request.UserRequest;
 import com.jleber.login.challenge.response.UserResponse;
+import com.jleber.login.challenge.service.UserInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-@Controller
+@RestController
 @RequestMapping("/users")
 public class UserController {
 
-    @PostMapping(value = "/authentication", consumes = APPLICATION_JSON_VALUE)
-    public ResponseEntity<UserResponse> registration(UserRequest userRequest) {
-        return null;
+    @Autowired
+    private UserInfoService userInfoService;
+
+    @PostMapping(value = "/authenticate", consumes = APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserResponse> authenticate(@RequestBody UserRequest userRequest) {
+        UserResponse response = userInfoService.authenticate(userRequest);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
