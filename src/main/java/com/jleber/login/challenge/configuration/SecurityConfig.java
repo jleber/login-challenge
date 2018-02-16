8ea923a1ca5b19e28ed880eee6f80e3d57b7e8d8
João Leber <joao.leber91@gmail.com>
2018-02-16 13:49:55 -0200

    improving security

--- src/main/java/com/jleber/login/challenge/configuration/SecurityConfig.java
+++ src/main/java/com/jleber/login/challenge/configuration/SecurityConfig.java
@@ -1,30 +1,36 @@
 package com.jleber.login.challenge.configuration;
 
+import com.jleber.login.challenge.filter.TokenAuthenticationFilter;
+import com.jleber.login.challenge.service.UserInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
-/**
- * Created by jleber on 14/02/18.
- */
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UserInfoService userInfoService;
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/users/authenticate");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+                .anyRequest().authenticated();
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(new TokenAuthenticationFilter(userInfoService), BasicAuthenticationFilter.class);
     }
 
 }
--- src/main/java/com/jleber/login/challenge/filter/TokenAuthenticationFilter.java
+++ src/main/java/com/jleber/login/challenge/filter/TokenAuthenticationFilter.java
@@ -1,7 +1,49 @@
 package com.jleber.login.challenge.filter;
 
-/**
- * Created by jleber on 16/02/18.
- */
-public class TokenAuthenticationFilter {
+import com.jleber.login.challenge.model.UserInfo;
+import com.jleber.login.challenge.service.UserInfoService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class TokenAuthenticationFilter extends GenericFilterBean {
+
+    private UserInfoService userInfoService;
+
+    public TokenAuthenticationFilter(UserInfoService userInfoService) {
+        this.userInfoService = userInfoService;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest)request;
+
+        String token = httpRequest.getHeader("api_token");
+
+        UserInfo userInfo = userInfoService.findByToken(token)
+                                        .orElseThrow(() -> new RuntimeException("Invalid Token !"));
+
+        UserDetails user = User.withUsername(userInfo.getEmail())
+                                .password(userInfo.getPassword())
+                                .credentialsExpired(false)
+                                .accountExpired(false)
+                                .accountLocked(false)
+                                .disabled(false)
+                                .authorities("API_USER")
+                                .build();
+
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        chain.doFilter(request, response);
+    }
 }
