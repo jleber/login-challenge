15e582930bfb4f276bd59b297539c8582041f4a5
João Leber <joao.leber91@gmail.com>
2018-02-16 13:55:50 -0200

    Adding exception handler

--- src/main/java/com/jleber/login/challenge/controller/advice/ExceptionResponseController.java
+++ src/main/java/com/jleber/login/challenge/controller/advice/ExceptionResponseController.java
@@ -1,7 +1,27 @@
 package com.jleber.login.challenge.controller.advice;
 
-/**
- * Created by jleber on 16/02/18.
- */
-public class ExceptionResponseController {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+@ControllerAdvice
+public class ExceptionResponseController extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {RuntimeException.class})
+    protected ResponseEntity<Object> handleException(RuntimeException e, WebRequest request) throws Exception {
+
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode node = mapper.createObjectNode();
+        node.put("Message", e.getMessage());
+        node.put("Status", "ERROR");
+
+        return handleExceptionInternal(e, mapper.writeValueAsString(node), new HttpHeaders(), INTERNAL_SERVER_ERROR, request);
+    }
 }
