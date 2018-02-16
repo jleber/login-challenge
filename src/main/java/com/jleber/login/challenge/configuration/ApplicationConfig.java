b71101f130d1d9f037a805460d3512c0d2da80f2
João Leber <joao.leber91@gmail.com>
2018-02-16 13:54:04 -0200

    Refatoring configs and add mapper bean

--- src/main/java/com/jleber/login/challenge/configuration/ApplicationConfig.java
+++ src/main/java/com/jleber/login/challenge/configuration/ApplicationConfig.java
@@ -1,7 +1,20 @@
 package com.jleber.login.challenge.configuration;
 
+import ma.glasnost.orika.MapperFacade;
+import ma.glasnost.orika.impl.DefaultMapperFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.jleber.login.challenge")
+@EnableJpaRepositories(basePackages = "com.jleber.login.challenge.repository")
+@EntityScan(basePackages = "com.jleber.login.challenge.model")
 public class ApplicationConfig {
+
+    @Bean
+    public MapperFacade mapperFacade(){
+        return new DefaultMapperFactory.Builder().build().getMapperFacade();
+    }
+
 }
