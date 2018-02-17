6bac7682ed3d049256e709c0f9d79ce8a88b7fe5
João Leber <joao.leber91@gmail.com>
2018-02-17 00:27:58 -0200

    Mapping products inside Company

--- src/main/java/com/jleber/login/challenge/model/Company.java
+++ src/main/java/com/jleber/login/challenge/model/Company.java
@@ -7,6 +7,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.util.List;
@@ -33,4 +36,16 @@ public class Company {
 
     @OneToMany(mappedBy="company")
     private List<UserInfo> usersInfo;
+
+    @ManyToMany
+    @JoinTable(
+            name="company_product",
+            joinColumns={
+                    @JoinColumn(name="id_company")
+            },
+            inverseJoinColumns={
+                    @JoinColumn(name="id_product")
+            }
+    )
+    private List<Product> products;
 }
