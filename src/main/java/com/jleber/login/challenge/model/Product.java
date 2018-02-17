2bed1d701ee269815ca8af8d7b2bc8491ddb9b7b
João Leber <joao.leber91@gmail.com>
2018-02-17 00:27:27 -0200

    Adding Product entity

--- src/main/java/com/jleber/login/challenge/model/Product.java
+++ src/main/java/com/jleber/login/challenge/model/Product.java
@@ -1,7 +1,19 @@
 package com.jleber.login.challenge.model;
 
-/**
- * Created by jleber on 16/02/18.
- */
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "product")
 public class Product {
+
+    @Id
+    private Long idProduct;
+    private String name;
+    private String type;
+
 }
