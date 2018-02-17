3d27ae4db5855906c673839ff1ae43c072f3f1e5
João Leber <joao.leber91@gmail.com>
2018-02-17 00:31:53 -0200

    Caching methods

--- src/main/java/com/jleber/login/challenge/service/CompanyService.java
+++ src/main/java/com/jleber/login/challenge/service/CompanyService.java
@@ -2,9 +2,13 @@ package com.jleber.login.challenge.service;
 
 import com.jleber.login.challenge.repository.CompanyRepository;
 import com.jleber.login.challenge.response.CompanyResponse;
+import com.jleber.login.challenge.response.ProductResponse;
 import ma.glasnost.orika.MapperFacade;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CompanyService {
@@ -18,9 +22,15 @@ public class CompanyService {
     }
 
 
+    @Cacheable(cacheNames = {"findCompany"},key = "#root.method.name + #token")
     public CompanyResponse findCompany(String token){
         return repository.findCompanyByUsersInfoToken(token)
-                    .map(company -> mapper.map(company, CompanyResponse.class))
-                    .orElseGet(() -> new CompanyResponse());
+                .map(company -> mapper.map(company, CompanyResponse.class))
+                .orElseGet(CompanyResponse::new);
+    }
+
+    @Cacheable(cacheNames = {"findCompanyProducts"},key = "#root.method.name + #idCompany")
+    public List<ProductResponse> findCompanyProducts(Long idCompany){
+        return mapper.mapAsList(repository.findOne(idCompany).getProducts(), ProductResponse.class);
     }
 }
