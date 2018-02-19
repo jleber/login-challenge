68db832f450b3faa663a2bcf571950b71c88181a
João Leber <joao.leber91@gmail.com>
2018-02-19 00:12:43 -0300

    Polish

--- src/main/java/com/jleber/login/challenge/filter/TokenAuthenticationFilter.java
+++ src/main/java/com/jleber/login/challenge/filter/TokenAuthenticationFilter.java
@@ -1,15 +1,9 @@
 package com.jleber.login.challenge.filter;
 
-import com.jleber.login.challenge.model.UserInfo;
-import com.jleber.login.challenge.response.UserResponse;
 import com.jleber.login.challenge.service.UserInfoService;
-import org.hibernate.Hibernate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.FilterChain;
