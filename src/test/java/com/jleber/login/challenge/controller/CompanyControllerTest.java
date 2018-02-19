9fdaef120d0be0a24ad883205883fb8d96078e69
João Leber <joao.leber91@gmail.com>
2018-02-19 00:16:57 -0300

    Adding integration tests

--- src/test/java/com/jleber/login/challenge/controller/CompanyControllerTest.java
+++ src/test/java/com/jleber/login/challenge/controller/CompanyControllerTest.java
@@ -1,7 +1,65 @@
 package com.jleber.login.challenge.controller;
 
-/**
- * Created by jleber on 18/02/18.
- */
+import com.jleber.login.challenge.configuration.ApplicationConfig;
+import com.jleber.login.challenge.repository.CompanyRepository;
+import com.jleber.login.challenge.repository.UserRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = RANDOM_PORT, classes = ApplicationConfig.class)
 public class CompanyControllerTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Test
+    public void shouldCompanyRequestReturnInfo() throws Exception {
+
+        String token = userRepository.findOne("mock@mock.com").getToken();
+
+        mockMvc.perform(get("/companies")
+                .header("api_token", token)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void shouldRequestWithoutTokenFail() throws Exception {
+        mockMvc.perform(get("/companies")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isInternalServerError());
+    }
+
+    @Test
+    public void shouldProductRequestReturnInfo() throws Exception {
+
+        Long idCompany = companyRepository.findOne(1L).getIdCompany();
+        String token = userRepository.findOne("mock@mock.com").getToken();
+
+        mockMvc.perform(get("/companies/"+idCompany+"/products")
+                .header("api_token", token)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
 }
--- src/test/java/com/jleber/login/challenge/controller/UserControllerTest.java
+++ src/test/java/com/jleber/login/challenge/controller/UserControllerTest.java
@@ -1,7 +1,52 @@
 package com.jleber.login.challenge.controller;
 
-/**
- * Created by jleber on 18/02/18.
- */
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.jleber.login.challenge.configuration.ApplicationConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = RANDOM_PORT, classes = ApplicationConfig.class)
 public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    public void shouldUserAuthenticate() throws Exception {
+        ObjectNode request = objectMapper.createObjectNode()
+                                         .put("email", "mock@mock.com")
+                                         .put("password", "secret");
+
+        mockMvc.perform(post("/users/authenticate")
+                .content(objectMapper.writeValueAsString(request))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldUserAuthenticationFail() throws Exception {
+        ObjectNode request = objectMapper.createObjectNode()
+                .put("email", "mock@mock2.com")
+                .put("password", "secret2");
+
+        mockMvc.perform(post("/users/authenticate")
+                .content(objectMapper.writeValueAsString(request))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isInternalServerError());
+    }
 }
