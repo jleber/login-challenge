a4b2209ddafe96785fb5fa124e1b1b093663b305
João Leber <joao.leber91@gmail.com>
2018-02-19 03:02:25 -0300

    Adding embedded redis for tests

--- src/test/java/com/jleber/login/challenge/AppTest.java
+++ src/test/java/com/jleber/login/challenge/AppTest.java
@@ -2,8 +2,13 @@ package com.jleber.login.challenge;
 
 import com.jleber.login.challenge.controller.CompanyControllerTest;
 import com.jleber.login.challenge.controller.UserControllerTest;
+import com.jleber.login.challenge.model.Company;
+import com.jleber.login.challenge.model.Product;
+import com.jleber.login.challenge.utils.TestDataCreator;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import redis.embedded.RedisServer;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
@@ -11,6 +16,10 @@ import org.junit.runners.Suite;
         CompanyControllerTest.class
 })
 public class AppTest {
-
+    @Before
+    public void setup() throws Exception {
+        RedisServer redisServer = new RedisServer(6379);
+        redisServer.start();
+    }
 
 }
